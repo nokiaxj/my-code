bd4be125dc279cbd258fe25883541809a01acbc7
nokiaxj <nokiaxj@gmail.com>
2026-06-18 23:09:36 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -382,7 +382,6 @@ generate_config() {
       "mtu": 1280,
       "address": [
         "172.16.0.2/32"
-		"2606:4700:110:8e62:2f62:eb69:3d97:c6a5/128"
       ],
       "private_key": "wBBUpigxbXdv8NGRLHD0BnMfBhHlfujf9s8/BG8BLVo=",
       "peers": [
